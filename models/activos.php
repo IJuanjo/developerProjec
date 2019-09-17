c7c20b3e258bfb7f162cd20ca39f8839ecef0530
juanjo <juanjobenavides314@gmail.com>
2019-09-17 16:53:03 -0500

    se modifico el controller

---
+++ models/activos.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Activos {
+
+	private $cod_maq ;
+	private $nombre ;
+	private $des_tecnica; 
+	private $fecha_compra ;
+	private $fecha_insta ;
+	private $vida_horas;
+}
+
+
+
+?>
