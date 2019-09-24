92f0ac3d0863fe07253cfc34c3d5c97f455a926d
Gers159 <maxp10190@gmail.com>
2019-09-24 16:02:37 -0500

    Se agrego nombres a los imput

--- controllers/activosController.php
+++ controllers/activosController.php
@@ -10,11 +10,13 @@ Class ActivosController{
         return require_once 'views/activos/agregar.php';
     }
     public function save(){
-        if($_POST){
-            var_dump($_POST);
-        }else{
-            echo "faild";
-        }
+       $nombre=isset($_POST['nombre'] ) ? $_POST['nombre'] : false;
+       $desc_tecnica=isset($_POST['descripcion'] ) ? $_POST['descripcion'] : false;
+       $fecha=isset($_POST['fecha'] ) ? $_POST['fecha'] : false;
+       $tiempo=isset($_POST['tiempo'] ) ? $_POST['tiempo'] : false;
+       $fecha2=isset($_POST['fecha2'] ) ? $_POST['fecha2'] : false;
+       $tiempo2=isset($_POST['tiempo2'] ) ? $_POST['tiempo2'] : false;
+       $horasv=isset($_POST['horasv'] ) ? $_POST['horasv'] : false;
     }
     
 }
--- views/activos/agregar.php
+++ views/activos/agregar.php
@@ -5,36 +5,37 @@
 		<div class="container">
 			<div class="row" id="contenedor01">
 				<div class="col" id="part-01">
-					<form action="POST">
+					<form action="<?=base_url?>activos/save" method="POST">
 						<div class="form-group">
-							<label id="espacio-lineas">Nombre:</label>
-							<input type="text" class="form-control" id="entrada-texto">
+						 	<label id="espacio-lineas">Nombre:</label>
+							<input type="text" class="form-control" id="entrada-texto" name="nombre">
 							<label id="espacio-lineas">Descripción técnica:</label>
-							<textarea class="form-control" id="caja-texto01"></textarea>
+							<textarea class="form-control" id="caja-texto01" name="descripcion"></textarea>
 							<label id="espacio-lineas">Fecha de compra:</label>
 							<div class="row">
 								<div class="col">
-									<input type="date" class="form-control" id="fecha-01">
+									<input type="date" class="form-control" id="fecha-01" name="fecha">
 								</div>
 								<div class="col">
-									<input type="time" class="form-control" id="tiempo-01">
+									<input type="time" class="form-control" id="tiempo-01" name="tiempo">
 								</div>
 							</div>
 							<label id="espacio-lineas">Fecha de instalación:</label>
 							<div class="row">
 									<div class="col">
-										<input type="date" class="form-control" id="fecha-01">
+										<input type="date" class="form-control" id="fecha-01" name="fecha2">
 									</div>
 									<div class="col">
-										<input type="time" class="form-control" id="tiempo-01">
+										<input type="time" class="form-control" id="tiempo-01" name="tiempo2">
 									</div>
 								</div>
 							<label id="espacio-lineas">Horas de vida:</label>
-							<input type="text" class="form-control" id="fecha-01">
-						</div>
+                            <input type="text" class="form-control" id="fecha-01" name="horasv">
+                            <input type="submit" class="btn btn-primary mt-4 d-block"> 
+                        </div>
 					</form>
 					<br/>
-					<a href="principal.html" role="button" class="btn btn-primary btn-lg" id="boton01">Guardar</a>
+				
 				</div>
 			</div>
 		</div>
